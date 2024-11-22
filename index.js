6e15dede4eb4c73388e6ec352982dcd2401aaec9
tanyashukla <shukla.tan@northeastern.edu>
2024-11-22 17:15:26 -0500

    updated server

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import Lab5 from "./Lab5/index.js";
-import Hello from "./Hello.js"
+import Hello from "./hello.js"
 import cors from "cors";
 import UserRoutes from "./Kanbas/Users/routes.js";
 import session from "express-session";
